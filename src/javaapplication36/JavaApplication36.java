ee2e4e184d5b3764ccb411fbfbeeee0d32032f97
Arya Gading Indratama <arya_gading_27rpl@student.smktelkom-mlg.sch.id>
2019-01-30 21:46:36 +0700

    Remedial 1

---
+++ src/javaapplication36/JavaApplication36.java
@@ -0,0 +1,20 @@
+package javaapplication36;
+
+import java.util.Scanner;
+
+public class JavaApplication36 {
+
+        public static void main(String[] args) {
+       
+int i;
+int j;
+
+for(i = 5; i >= 1; i--) {
+    for(j = 1; j <= i; j++) {
+        System.out.print(j);
+            }
+    System.out.println();
+        
+    }
+}
+}
